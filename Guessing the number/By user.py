b900057da7ee766ed730f05b5d5976c8799f0881
prachihooda06 <95120511+prachihooda06@users.noreply.github.com>
2022-06-16 21:29:03 +0530

    Update By user.py

--- Guessing the number/By user.py
+++ Guessing the number/By user.py
@@ -1,1 +1,16 @@
+import random
 
+
+def guess_no(a):
+    random_number = random.randint(1, a)
+    guess_no = 0
+    while guess_no != random_number:
+        guess_no = int(input(f'Guess a number between 1 and {a}: '))
+        if guess_no < random_number:
+            print('Sorry, guess again too low.')
+        elif guess_no > random_number:
+            print('Sorry, guess again too high.')
+        else:
+            print(f'Yayy! You guessed it correct.')
+
+guess_no(10)
